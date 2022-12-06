38f4a74afb7692fe71bcbfa0c4c1b21bebcd4364
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2022-12-06 19:31:20 +0300

    Домашняя работа 2 декабря - первое web приложение "Рецепты"

---
+++ src/main/java/skypro/learn/recipewebfirstapp/RecipeWebFirstAppApplication.java
@@ -0,0 +1,13 @@
+package skypro.learn.recipewebfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeWebFirstAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeWebFirstAppApplication.class, args);
+    }
+
+}
